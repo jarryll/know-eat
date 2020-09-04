a4d45539881656453d361339a9a35a8054e0fbdc
jarryl <jarryl.chia@gmail.com>
2020-09-04 11:38:16 +0800

    Done up cookie authentication feature to track sessions

--- controllers/users.js
+++ controllers/users.js
@@ -4,7 +4,20 @@ const hash = require('js-sha256');
 
 module.exports = (db) => {
 
-    let test = (req, res) => res.render('login')
+    let displayMain = (req, res) => {
+        if (!req.cookies['loggedIn']) {res.render('login')}
+        else {
+            let userIdCookie = req.cookies['user_id']
+            let loggedInCookie = req.cookies['loggedIn']
+            console.log (userIdCookie);
+            console.log(loggedInCookie)
+            if (loggedInCookie === hash(`${SALT}-${userIdCookie}-true`)) {res.render ('main')}
+            else {
+                res.clearCookie('loggedIn');
+                res.render('login')
+            }
+        }
+    }
 
     let checkCredentials = async (req, res) => {
         let values = [req.body.username, hash(req.body.password)];
@@ -13,7 +26,7 @@ module.exports = (db) => {
             if (result.rowCount > 0) {
                 res.cookie('loggedIn', hash(`${SALT}-${hash(`${result.rows[0]["id"]}`)}-true`))
                 res.cookie('user_id', hash(`${result.rows[0]['id']}`))
-                res.render('main')
+                res.redirect('/')
             } else {
                 res.send('please create an account')
             }
@@ -23,7 +36,7 @@ module.exports = (db) => {
     }
 
     return {
-        test,
+        displayMain,
         checkCredentials
     }
 }
--- routes.js
+++ routes.js
@@ -2,7 +2,7 @@ module.exports = (app, allModels) => {
   // require the controllers
   const usersCallbacks = require('./controllers/users')(allModels);
   // routes
-  app.get('/', usersCallbacks.test);
+  app.get('/', usersCallbacks.displayMain);
   app.post('/user', usersCallbacks.checkCredentials)
 
   };
