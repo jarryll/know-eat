07ce1de6891b37100c65e16b30b8536fbbf9a973
jarryl <jarryl.chia@gmail.com>
2020-09-07 00:33:46 +0800

    did up bootstrap and css stylings for login and main pages

--- views/login.jsx
+++ views/login.jsx
@@ -8,12 +8,16 @@ export default class Login extends React.Component {
                     <meta charSet="utf-8"/>
                     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
                     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossOrigin="anonymous"/>
-                    <link rel="stylesheet" type="text/css" href="css/style.css"/>
+                    <link rel="stylesheet" type="text/css" href="css/login-style.css"/>
+                    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,600;1,400&display=swap" rel="stylesheet"/>
+                    <title>knowEat! - Know what you eat</title>
                 </head>
                 <body>
                     <div className="container">
-                    <h2 className="text-center">Know Eat!</h2>
-                    <p className="text-center">Keep track of what you eat</p>
+                        <div className="logo-container">
+                            <img src="./images/login-logo.png" className="logo"/>
+                        </div>
+                        <div className="form-container">
                     <form method="POST" action="/user">
                         <div className="form-group">
                             <label htmlFor="username">Username</label>
@@ -23,11 +27,17 @@ export default class Login extends React.Component {
                             <label htmlFor="password">Password</label>
                             <input type="password" name="password" className="form-control form-control-lg" required/>
                         </div>
-                        <button type="submit" id="signIn" className="btn btn-primary btn-lg btn-block">Sign in</button>
+                        <div className="signin-container">
+                        <button type="submit" id="signIn" className="btn btn-primary btn-block btn-lg btn-custom">Sign in</button>
+                        </div>
                     </form>
+
+                    <div className = "register-container">
                     <form method="GET" action="/register">
-                        <button type="submit" id="register">Register</button>
+                        <button type="submit" id="register" className="btn btn-primary btn-lg btn-block btn-custom">Register</button>
                     </form>
+                    </div>
+                    </div>
                     </div>
                 </body>
             </html>
--- views/main.jsx
+++ views/main.jsx
@@ -30,12 +30,26 @@ export default class Main extends React.Component{
                 <meta charSet="utf-8"/>
                     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
                     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossOrigin="anonymous"/>
-                    <link rel="stylesheet" type="text/css" href="css/style.css"/>
+                    <link rel="stylesheet" type="text/css" href="css/main-style.css"/>
+                    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,600;1,400&display=swap" rel="stylesheet"/>
+                    <title>knowEat! - Know what you eat</title>
             </head>
             <body>
-            <div className = "container">
-                <h1>Welcome, {userNameCookie}</h1>
-                <h2>Here's your food log for today, {date}:</h2>
+            <div className = "container-fluid">
+             <nav className="navbar navbar-light fixed-top justify-content-between navbar-custom">
+                    <img className ="logo" src="./images/logo_resized.png"/>
+                    <form className="form-inline" method="GET" action="/logout">
+                        <button className="btn btn-outline-light" type="submit">Sign out</button>
+                    </form>
+                </nav>
+            </div>
+
+            <div className = "container main-container">
+
+                <h1 id="welcome">Welcome, {userNameCookie}.</h1>
+
+                <div className="food-log">
+                <h2 id="date-dashboard">Here's your food log for today, <span id="date">{date}</span>:</h2>
 
                  <div>
                     <table className = "table table-striped table-bordered table-hover">
@@ -55,25 +69,22 @@ export default class Main extends React.Component{
 
                  <div className="totalCaloriesDisplay">
                  </div>
-
-                <div>
+                 </div>
+                <div className="add-food form-group">
                     <form method="POST" action="/addItem">
                         <div>
                             <div>
                                 <label htmlFor ="foodItem">Food Item</label>
-                                <input type ="text" id="foodInput" name="foodItem"/>
+                                <input type ="text" className="form-control" id="foodInput" name="foodItem" placeholder="What did you eat today?"/>
                             </div>
-                            <div>
+                            <div className="notes">
                                 <label htmlFor ="notes">Notes</label>
-                                <textarea rows="4" cols="60" id="notes" name="notes" placeholder="Add your thoughts about what you ate..."/>
+                                <textarea rows="4" cols="60" id="notes" className="form-control" name="notes" placeholder="Add your thoughts here!"/>
                             </div>
-                            <div>
-                                <button type="submit" id="foodInputButton">Log it!</button>
+                            <div className="log-food-btn">
+                                <button type="submit" className="btn btn-primary btn-lg btn-block" id="foodInputButton">Log it!</button>
                             </div>
                         </div>
-                    </form>
-                    <form method="GET" action="/logout">
-                        <button type="submit" id="register">Log out</button>
                     </form>
                 </div>
                 <script src="script.js" type="text/javascript"></script>
